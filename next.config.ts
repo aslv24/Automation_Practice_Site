6827b2b48c8dcc17f3a0f2021ec2b1f31e0268d6
aslv24 <aslinfomats@gmail.com>
2026-03-19 17:45:21 +0530

    Update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // ✅ Add this line for static export
+  output: "export",
+
+  // ✅ Optional but IMPORTANT for GitHub Pages
+  basePath: "/Automation_Practice_Site",
+  assetPrefix: "/Automation_Practice_Site/",
 };
 
 export default nextConfig;
