ee151b6205e882e1c15bd422199d7a1b9f0c8794
aslv24 <aslinfomats@gmail.com>
2026-03-19 17:56:52 +0530

    Prepare docs folder for GitHub Pages hosting

---
+++ docs/_next/static/WHSkqY4ljQUMGpF2IFwjc/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Automation_Practice_Site//_next/:path+",
+        "destination": "/Automation_Practice_Site/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
